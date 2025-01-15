3e7f6c4bd5bd5675076dad2b6f3d058fe2a97ffe
Stebane Jhon <111849849+StebaneJhon@users.noreply.github.com>
2025-01-15 19:57:01 +0100

    app icon update

--- app/src/main/java/com/ssoaharison/mypasswordmanager/ui/theme/Theme.kt
+++ app/src/main/java/com/ssoaharison/mypasswordmanager/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.ssoaharison.mypasswordmanager.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -12,28 +11,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Immutable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
-
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
-)
 
 private val lightScheme = lightColorScheme(
     primary = primaryLight,
@@ -278,8 +255,7 @@ val unspecified_scheme = ColorFamily(
 @Composable
 fun MyPasswordManagerTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = false,  // To be changed later
+    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
